3f07fcbeb26d385e13fb641e2f072ad2b950b0e4
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-25 15:08:29 +0700

    jhgf

--- function.php
+++ function.php
@@ -55,53 +55,7 @@ $header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.6631".mt_rand(100,999).",115.21".mt_rand(1000,9999);
-if ($pin):
-$header[] = "pin: $pin";
-    endif;
-if ($token):
-$header[] = "Authorization: Bearer $token";
-endif;
-if ($otpsetpin):
-$header[] = "otp: $otpsetpin";
-endif;
-if ($uuid):
-$header[] = "User-uuid: $uuid";
-endif;
-$c = curl_init("https://api.gojekapi.com".$url);
-    curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-    if ($data):
-    curl_setopt($c, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($c, CURLOPT_POST, true);
-    endif;
-    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($c, CURLOPT_HEADER, true);
-    curl_setopt($c, CURLOPT_HTTPHEADER, $header);
-    $response = curl_exec($c);
-    $httpcode = curl_getinfo($c);
-    if (!$httpcode)
-        return false;
-    else {
-        $header = substr($response, 0, curl_getinfo($c, CURLINFO_HEADER_SIZE));
-        $body   = substr($response, curl_getinfo($c, CURLINFO_HEADER_SIZE));
-    }
-    $json = json_decode($body, true);
-    return $body;
-}
-
-function request1($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
-$header[] = "Host: api.gojekapi.com";
-$header[] = "User-Agent: okhttp/3.10.0";
-$header[] = "Accept: application/json";
-$header[] = "Accept-Language: id-ID";
-$header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.46.1";
-$header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
-$header[] = "Connection: keep-alive";
-$header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.5288".mt_rand(100,999).",115.21".mt_rand(1000,9999);
+$header[] = "X-Location: -8.6918".mt_rand(100,999).",115.21".mt_rand(1000,9999);
 if ($pin):
 $header[] = "pin: $pin";
     endif;
